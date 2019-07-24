d888ed4ae40a4a63c2c429888aba095cd27526f5
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:43:21 +0200

    :bookmark: v2.0.17
    Add jsdelivr

--- dist/split-if-boxed-bug-x.js
+++ dist/split-if-boxed-bug-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-24T00:09:37.357Z",
+  "date": "2019-07-24T13:43:15.966Z",
   "describe": "",
   "description": "Tests if a value is a string with the boxed bug; splits to an array.",
   "file": "split-if-boxed-bug-x.js",
   "hash": "95351761c30422b1164a",
   "license": "MIT",
-  "version": "2.0.16"
+  "version": "2.0.17"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
