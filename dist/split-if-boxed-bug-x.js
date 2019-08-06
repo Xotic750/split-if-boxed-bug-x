f20a1ac3d3cbadfa6c37c9280a19d6164cd7e28e
Graham Fairweather <xotic750@gmail.com>
2019-08-06 23:00:17 +0200

    Add coveralls badge

--- dist/split-if-boxed-bug-x.js
+++ dist/split-if-boxed-bug-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-08-06T00:33:32.853Z",
+  "date": "2019-08-06T21:00:12.011Z",
   "describe": "",
   "description": "Tests if a value is a string with the boxed bug; splits to an array.",
   "file": "split-if-boxed-bug-x.js",
