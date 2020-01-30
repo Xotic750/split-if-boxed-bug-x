91733298f0cf442c937698cb87a7c98e47d1b0fb
Graham Fairweather <xotic750@gmail.com>
2020-01-30 12:25:12 +0100

    Maintenance

--- dist/split-if-boxed-bug-x.js
+++ dist/split-if-boxed-bug-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-09-05T18:29:23.321Z",
+  "date": "2020-01-30T11:24:55.566Z",
   "describe": "",
   "description": "Tests if a value is a string with the boxed bug; splits to an array.",
   "file": "split-if-boxed-bug-x.js",
